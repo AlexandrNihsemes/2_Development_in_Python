c807035d0edb68d182515d6e0aaea9af38ff9a2c
Alexandr Semeshin <nihsemes2014@mail.ru>
2026-06-12 19:37:20 +0500

    filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -3,6 +3,18 @@ def filter_by_state(dictionary_list: list, state="EXECUTED") -> list:
     (по умолчанию 'EXECUTED' и возвращает новый список словарей,
     содержащий только те словари, у которых ключ state соответствует указанному значению.)"""
 
+    executed_list = []  # Список для хранения словарей с состоянием EXECUTED
+    canceled_list = []  # Список для хранения словарей с состоянием CANCELED
+
+    for item in dictionary_list:
+
+        if item["state"] == state:
+            executed_list.append(item)
+        elif item["state"] == "CANCELED":
+            canceled_list.append(item)
+
+    return executed_list, canceled_list
+
 
 
 
