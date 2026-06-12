70685cd09bb3e94f2aaace9d1b8b3d7cf32a75ed
Alexandr Semeshin <nihsemes2014@mail.ru>
2026-06-12 23:05:43 +0500

    examination_01

--- src/processing.py
+++ src/processing.py
@@ -4,16 +4,13 @@ def filter_by_state(dictionary_list: list, state="EXECUTED") -> list:
     содержащий только те словари, у которых ключ state соответствует указанному значению.)"""
 
     executed_list = []  # Список для хранения словарей с состоянием EXECUTED
-    canceled_list = []  # Список для хранения словарей с состоянием CANCELED
 
     for item in dictionary_list:
 
         if item["state"] == state:
             executed_list.append(item)
-        elif item["state"] == "CANCELED":
-            canceled_list.append(item)
 
-    return executed_list, canceled_list
+    return executed_list
 
 
 def sort_by_date(dictionary_list: list, parameter=True) -> list:
